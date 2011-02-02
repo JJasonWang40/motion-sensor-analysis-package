61c080558be1da2cb336d8a29043145995a56629
ruppa <ruppa@in.tum.de>
2011-02-02 11:01:50 +0000

    Anlegen der GUI Struktur zum Speichern der neuen XML Daten Dateien.

--- ActigraphAuswertung/Form1.cs
+++ ActigraphAuswertung/Form1.cs
@@ -497,5 +497,27 @@ namespace ActigraphAuswertung
             catch { }
         }
         #endregion
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog loadDialog = new OpenFileDialog();
+            loadDialog.InitialDirectory = "Desktop";
+            loadDialog.Filter = "XML File|*.xml";
+            loadDialog.Title = "Save XML";
+            loadDialog.ShowDialog();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveAs = new SaveFileDialog();
+            SaveAs.Filter = "XML|*.xml";
+            SaveAs.Title = "Daten speichern unter:";
+            SaveAs.ShowDialog();
+        }
     }
 }
