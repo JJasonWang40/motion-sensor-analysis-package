ce00f89f842205872b619dd4ad84050dff43f545
ruppa@in.tum.de <ruppa@in.tum.de@66a3368c-b06d-9b7f-f19b-101cbc61171d>
2011-04-29 20:34:27 +0000

    Bugfix in Form1; Created the class for DB. At the moment is the constructor not available!

---
+++ trunk/ActigraphAuswertung/Model/DBCsvModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ActigraphAuswertung.Model
+{
+    class DBCsvModel 
+    {
+        DataSet DB = new DataSet();
+
+        //public DBCsvModel(){
+        //    DataTable files = new DataTable();
+        //    files.Columns.Add("Absolute Filename", typeof(String));
+        //    files.Columns.Add("Name",typeof(String));
+        //    DataTable dataset = new DataTable();
+        //    dataset.Constraints.Add("files",files.Columns[0],false);
+        //    dataset.Columns.Add("Date", typeof(DateTime));
+        //    dataset.Columns.Add("Activity", typeof(Int32));
+        //    dataset.Columns.Add("ActivityY", typeof(Int32));
+        //    dataset.Columns.Add("ActivityZ", typeof(Int32));
+        //    dataset.Columns.Add("Incilometer", typeof(Int32));
+        //    dataset.Columns.Add("Steps", typeof(Int32));
+        //    dataset.Columns.Add("VMU", typeof(Int32));
+        //    dataset.Columns.Add("CaloriesActivity", typeof(float));
+        //    dataset.Columns.Add("CaloriesTotal", typeof(float));
+        //    DBCsvModel.Tables.Add(files);
+        //    DBCsvModel.Tables.Add(dataset);
+        //}
+
+        
+    }
+}
